df7c586a47a2482048ab98a83a581112276a2c57
smssyquia <54983967+Deadify@users.noreply.github.com>
2020-04-14 16:59:30 +0800

    Placed Ideas file in the proper directory

--- src/components/Ideas.jsx
+++ src/components/Ideas.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div className="form">
-      <title>Idea?</title>
+      <title>Donate to Fight COVID-19</title>
       <iframe
         title="Ideas Link Google Form"
         src="https://docs.google.com/forms/d/e/1FAIpQLSex9qU519nOfljebYKqSBzGfy-JZo1uTAVNzWd_waXtNDrUbQ/viewform?embedded=true"
