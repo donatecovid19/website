8b6111749dfa7ba9e8e19e2699d504218db7be4a
smssyquia <54983967+Deadify@users.noreply.github.com>
2020-04-20 12:27:57 +0800

    Added HTML form to replace "share" Google Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,14 +2,64 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="form">
+    <div class="donation_form">
       <title>Donate to Fight COVID-19</title>
-      <iframe
-        title="Donation Link Google Form"
-        src="https://docs.google.com/forms/d/e/1FAIpQLSdvsS4TEJiDKwphNyaMeoBa4d8XbjsIvQd2hjucI1Ev5p5q5w/viewform?embedded=true"
-      >
-        Loading…
-      </iframe>
+      <header>
+        <h1>Donate to Fight COVID-19 &hearts;</h1>
+        <p>Share how we can help your country fight COVID-19!</p>
+      </header>
+      <form>
+        <div class="group">
+          <label for="region">Region</label>
+          <select id="region" name="region" class="field" required>
+            <option disabled selected value>
+              Select an option
+            </option>
+            <option value="global">Global</option>
+            <option value="africa">Africa</option>
+            <option value="asia">Asia</option>
+            <option value="central_america">Central America</option>
+            <option value="europe">Europe</option>
+            <option value="middle_east">Middle East</option>
+            <option value="north_america">North America</option>
+            <option value="oceania">Oceania</option>
+            <option value="south_america">South America</option>
+          </select>
+        </div>
+
+        <div class="group">
+          <label for="country">Country</label>
+          <p class="desc">*Please include city/state if applicable.</p>
+          <input
+            type="text"
+            name="country"
+            id="country"
+            class="field"
+            placeholder="Enter a country"
+          />
+        </div>
+
+        <div class="group">
+          <label for="link">Link</label>
+          <p class="desc">
+            *Please provide a direct link to a donation fund for COVID-19
+          </p>
+          <input
+            type="url"
+            name="link"
+            id="link"
+            class="field"
+            placeholder="Enter a link"
+            required
+          />
+        </div>
+
+        <div class="group">
+          <button type="submit" class="submit-button">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
