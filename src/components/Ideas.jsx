5bdb47a2639ea41bcd471156377a431123c0476e
Deadify <54983967+Deadify@users.noreply.github.com>
2020-04-28 12:59:43 +0800

    Revert "Vanilla HTML and CSS for "Ideas" form"
    This reverts commit 4615241a7fa70d30f00941a9a3f2dbfb957966a4.

--- src/components/Ideas.jsx
+++ src/components/Ideas.jsx
@@ -1,53 +1,15 @@
 import React from "react";
-import "./Form.css";
 
 function Home() {
   return (
-    <div class="donation_form">
+    <div className="form">
       <title>Donate to Fight COVID-19</title>
-      <header>
-        <h1>Share any ideas to help us improve our website!</h1>
-      </header>
-      <form>
-        <div class="group">
-          <label id="ideas" for="ideas">
-            What's up?
-          </label>
-          <input
-            type="text"
-            name="country"
-            class="field"
-            placeholder="Enter any idea"
-          />
-        </div>
-
-        <div class="group">
-          <label id="contact" for="contact">
-            Contact
-          </label>
-          <p class="desc">
-            If possible, please leave your email, Instagram, or any contact
-            method for us to learn more about your ideas!
-          </p>
-          <input
-            type="text"
-            name="contact"
-            class="field"
-            placeholder="Enter a contact method"
-          />
-        </div>
-
-        <div class="thanks">
-          <h2>Thanks for helping us fight COVID-19!</h2>
-        </div>
-        <br />
-
-        <div class="group">
-          <button type="submit" class="submit-button">
-            Submit
-          </button>
-        </div>
-      </form>
+      <iframe
+        title="Ideas Link Google Form"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSex9qU519nOfljebYKqSBzGfy-JZo1uTAVNzWd_waXtNDrUbQ/viewform?embedded=true"
+      >
+        Loading…
+      </iframe>
     </div>
   );
 }
--- src/components/home_page/Home.jsx
+++ src/components/home_page/Home.jsx
@@ -9,11 +9,7 @@ const GLOBAL = [
   { name: "Children's Hunger Fund", url: "https://bit.ly/donateCHFcovid19" },
   { name: "Unicef", url: "https://uni.cf/2V8fODA" },
   { name: "United Nations", url: "https://bit.ly/donateUNcovid19" },
-  {
-    name: "United Way",
-    url:
-      "https://secure.unitedway.org/j/step/covid19-donate?source=landingpage&subsource=covid19&utm_source=landingpage&utm_medium=web&utm_campaign=covid19&utm_content=donate",
-  },
+  { name: "United Way", url: "https://secure.unitedway.org/j/step/covid19-donate?source=landingpage&subsource=covid19&utm_source=landingpage&utm_medium=web&utm_campaign=covid19&utm_content=donate" },
   { name: "World Health Organization", url: "http://bit.ly/donateWHO" },
 ];
 
@@ -21,41 +17,20 @@ const ASIA = [
   { name: "Afghanistan", url: "http://bit.ly/donateAFcovid19" },
   { name: "China", url: "http://bit.ly/donateCNcovid19" },
   { name: "Hong Kong (Red Cross)", url: "http://bit.ly/donateHKcovid19" },
-  {
-    name: "Hong Kong (Save the Children)",
-    url: "http://bit.ly/donateSTCcovid19",
-  },
-  {
-    name: "Japan",
-    url: "https://www.plan-international.jp/news/info/20200331_21914/",
-  },
+  { name: "Hong Kong (Save the Children)", url: "http://bit.ly/donateSTCcovid19" },
+  { name: "Japan", url: "https://www.plan-international.jp/news/info/20200331_21914/" },
   { name: "India (Give)", url: "http://bit.ly/donateGIcovid19" },
   { name: "India (Rapid Response)", url: "http://bit.ly/donateINDcovid19" },
-  {
-    name: "Indonesia (Dompet Dhuafa)",
-    url: "https://donasi.dompetdhuafa.org/bersamalawancorona/",
-  },
-  {
-    name: "Indonesia (Unicef Indonesia)",
-    url: "http://bit.ly/donateINcovid19",
-  },
+  { name: "Indonesia (Dompet Dhuafa)", url: "https://donasi.dompetdhuafa.org/bersamalawancorona/" },
+  { name: "Indonesia (Unicef Indonesia)", url: "http://bit.ly/donateINcovid19" },
   { name: "Korea", url: "https://www.plankorea.or.kr/news/event/view/5043" },
-  {
-    name: "Malayasia (Kitajagakita)",
-    url: "https://kitajagakita.com/pages/submit-an-initiative-for-listing",
-  },
+  { name: "Malayasia (Kitajagakita)", url: "https://kitajagakita.com/pages/submit-an-initiative-for-listing" },
   { name: "Malaysia (Masterlist)", url: "http://bit.ly/donateMYcovid2019" },
-  {
-    name: "Malaysia (MERCY)",
-    url: "https://www.mercy.org.my/donate/wakaf-covid19/",
-  },
+  { name: "Malaysia (MERCY)", url: "https://www.mercy.org.my/donate/wakaf-covid19/" },
   { name: "Pakistan", url: "http://bit.ly/donatePKcovid19" },
   { name: "Philippines (Kaya Natin)", url: "http://bit.ly/donatePHcovid19" },
   { name: "Philippines (Masterlist)", url: "http://bit.ly/donatePHIcovid19" },
-  {
-    name: "Philippines (Project Pearls)",
-    url: "https://www.projectpearls.org/donate/",
-  },
+  { name: "Philippines (Project Pearls)", url: "https://www.projectpearls.org/donate/" },
   { name: "Singapore", url: "http://bit.ly/donateSGcovid19" },
 ];
 
@@ -63,30 +38,14 @@ const EUROPE = [
   { name: "Belgium", url: "https://donate.kbs-frb.be/Covid-19/~mon-don?_cv=1" },
   { name: "Denmark", url: "https://planbornefonden.dk/corona/" },
   { name: "Finland", url: "https://lahjoita.plan.fi/lahjoitus" },
-  {
-    name: "France",
-    url: "https://formulaire.plan-international.fr/covid19/~mon-don",
-  },
+  { name: "France", url: "https://formulaire.plan-international.fr/covid19/~mon-don" },
   { name: "Germany (Red Cross)", url: "http://bit.ly/donateDEcovid19" },
-  {
-    name: "Germany (Maecenata)",
-    url: "https://web.maecenata.eu/spenden?fb_item_id_fix=27166/",
-  },
+  { name: "Germany (Maecenata)", url: "https://web.maecenata.eu/spenden?fb_item_id_fix=27166/" },
   { name: "Italy (Red Cross)", url: "http://bit.ly/donateITcovid19" },
-  {
-    name: "Italy (Protezione Civile)",
-    url:
-      "http://www.protezionecivile.gov.it/attivita-rischi/rischio-sanitario/emergenze/coronavirus",
-  },
-  {
-    name: "Italy (Regione del Veneto)",
-    url: "https://www.regione.veneto.it/article-detail?articleId=4335623",
-  },
+  { name: "Italy (Protezione Civile)", url: "http://www.protezionecivile.gov.it/attivita-rischi/rischio-sanitario/emergenze/coronavirus" },
+  { name: "Italy (Regione del Veneto)", url: "https://www.regione.veneto.it/article-detail?articleId=4335623" },
   { name: "Netherlands (Red Cross)", url: "http://bit.ly/donateNLcovid19" },
-  {
-    name: "Netherlands (Plan International)",
-    url: "https://www.planinternational.nl/forms/stop-verspreiding-coronavirus",
-  },
+  { name: "Netherlands (Plan International)", url: "https://www.planinternational.nl/forms/stop-verspreiding-coronavirus" },
   { name: "Norway", url: "https://www.facebook.com/donate/2531936430404492/" },
   { name: "Poland", url: "http://bit.ly/donatePLcovid19" },
   { name: "Portugal (Go Parity)", url: "http://bit.ly/donatePTGcovid19" },
@@ -102,78 +61,28 @@ const NORTH_AMERICA = [
   { name: "Atlanta, GA (Emory)", url: "http://bit.ly/donateEMcovid19" },
   { name: "Atlanta, GA (Food Bank)", url: "http://bit.ly/donateATLcovid19" },
   { name: "Austin, TX", url: "http://bit.ly/donateATXcovid19" },
-  {
-    name: "Boone, IA",
-    url: "https://www.redcross.org/donate/donation.html/?donamt=0",
-  },
-  {
-    name: "Chicago, IL (Chicago's Food Bank)",
-    url: "http://bit.ly/donateCILcovid19",
-  },
-  {
-    name: "Chicago, IL (Community Fund)",
-    url: "https://www.chicagocovid19responsefund.org/",
-  },
-  {
-    name: "Chicago, IL (Feeding America)",
-    url: "https://www.feedingamerica.org/",
-  },
+  { name: "Boone, IA", url: "https://www.redcross.org/donate/donation.html/?donamt=0" },
+  { name: "Chicago, IL (Chicago's Food Bank)", url: "http://bit.ly/donateCILcovid19" },
+  { name: "Chicago, IL (Community Fund)", url: "https://www.chicagocovid19responsefund.org/" },
+  { name: "Chicago, IL (Feeding America)", url: "https://www.feedingamerica.org/" },
   { name: "Houston, TX", url: "http://bit.ly/donateHTXcovid19" },
   { name: "Illinois", url: "http://bit.ly/donateNILcovid19" },
   { name: "Lexington, KY", url: "https://bgcf.givingfuel.com/coronavirus" },
   { name: "Los Angeles, CA", url: "http://bit.ly/donateLAcovid19" },
-  {
-    name: "New York City, NY (CityMeals)",
-    url: "http://bit.ly/donateNYcovid19",
-  },
-  {
-    name: "New York City, NY (Mount Sinai)",
-    url:
-      "https://giving.mountsinai.org/site/Donation2;jsessionid=00000000.app216a?idb=1183495007&df_id=2021&mfc_pref=T&2021.donation=form1&NONCE_TOKEN=7E563239AF1564122679453358A75C21&utm_campaign=flowcodecovidgift&utm_source=flowcode&set.custom.appeal1=COVIDFLOWCODE&utm_medium=flowcode",
-  },
-  {
-    name: "New York City, NY (NYC)",
-    url: "https://www1.nyc.gov/site/fund/donate/donate.page",
-  },
+  { name: "New York City, NY (CityMeals)", url: "http://bit.ly/donateNYcovid19" },
+  { name: "New York City, NY (Mount Sinai)", url: "https://giving.mountsinai.org/site/Donation2;jsessionid=00000000.app216a?idb=1183495007&df_id=2021&mfc_pref=T&2021.donation=form1&NONCE_TOKEN=7E563239AF1564122679453358A75C21&utm_campaign=flowcodecovidgift&utm_source=flowcode&set.custom.appeal1=COVIDFLOWCODE&utm_medium=flowcode" },
   { name: "Pittsburgh, PA", url: "http://bit.ly/donatePITcovid19" },
-  {
-    name: "San Francisco, CA",
-    url: "https://sf.gov/give-city-respond-covid-19",
-  },
-  {
-    name: "Seattle, WA (University of Washington)",
-    url: "http://bit.ly/donateWAcovid19",
-  },
-  {
-    name: "Seattle, WA (Seattle Foundation)",
-    url: "http://bit.ly/donateUWcovid19",
-  },
+  { name: "San Francisco, CA", url: "https://sf.gov/give-city-respond-covid-19" },
+  { name: "Seattle, WA (University of Washington)", url: "http://bit.ly/donateWAcovid19" },
+  { name: "Seattle, WA (Seattle Foundation)", url: "http://bit.ly/donateUWcovid19" },
   { name: "Washington, D.C.", url: "https://www.northwestharvest.org/" },
-  {
-    name: "Canada (CanadaHelps)",
-    url:
-      "https://www.canadahelps.org/en/cause-funds/covid-19-community-care-fund/",
-    isCanada: true,
-  },
-  {
-    name: "Canada (Food Banks)",
-    url: "https://www.foodbankscanada.ca/COVID-19.aspx",
-    isCanada: true,
-  },
-  {
-    name: "Canada (Red Cross)",
-    url: "http://bit.ly/donateCNDcovid19",
-    isCanada: true,
-  },
+  { name: "Canada (Red Cross)", url: "http://bit.ly/donateCNDcovid19", isCanada: true },
+  { name: "Canada (CanadaHelps)", url: "https://www.canadahelps.org/en/cause-funds/covid-19-community-care-fund/", isCanada: true },
 ];
 
 const CENTRAL_AMERICA = [
   { name: "Aruba", url: "http://bit.ly/donateARcovid19" },
   { name: "Dominican Republic", url: "http://bit.ly/donateDOcovid19" },
-  {
-    name: "Mexico",
-    url: "https://www.cruzrojamexicana.org.mx/causa/detalle/34",
-  },
 ];
 
 const SOUTH_AMERICA = [
@@ -267,29 +176,13 @@ function Home() {
         <Box name="Asia" list_of_links={ASIA} className="asia" />
         <Box name="Europe" list_of_links={EUROPE} className="europe" />
 
-        <NorthAmerica
-          name="North America"
-          list_of_links={NORTH_AMERICA}
-          className="northamerica"
-        />
-        <Box
-          name="Central America"
-          list_of_links={CENTRAL_AMERICA}
-          className="centralamerica"
-        />
-        <Box
-          name="South America"
-          list_of_links={SOUTH_AMERICA}
-          className="southamerica"
-        />
+        <NorthAmerica name="North America" list_of_links={NORTH_AMERICA} className="northamerica" />
+        <Box name="Central America" list_of_links={CENTRAL_AMERICA} className="centralamerica" />
+        <Box name="South America" list_of_links={SOUTH_AMERICA} className="southamerica" />
 
         <Box name="Africa" list_of_links={AFRICA} className="africa" />
         <Box name="Oceania" list_of_links={OCEANIA} className="oceania" />
-        <Box
-          name="Middle East"
-          list_of_links={MIDDLE_EAST}
-          className="middleeast"
-        />
+        <Box name="Middle East" list_of_links={MIDDLE_EAST} className="middleeast" />
       </div>
     </div>
   );
