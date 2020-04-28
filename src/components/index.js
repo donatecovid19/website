29d83bf879ac84c222d6e8b3b4925dc8a7b8263f
Michael Cao <mike.x.cao@gmail.com>
2020-04-28 10:21:12 -0400

    moved the about page to a subfolder
    our components folder is getting weirder and weirder by the day... also I'm enjoying these commit submessages way too much

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,6 @@ export { default as Navigation } from "./Navigation";
 export { default as Home } from "./home_page/Home.jsx";
 export { default as Footer } from "./Footer";
 export { default as Form } from "./Form";
-export { default as About } from "./About";
+export { default as About } from "./home_page/About";
 export { default as Ideas } from "./Ideas";
 export { default as Main } from "./Main";
