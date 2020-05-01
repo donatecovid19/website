12d595c8305f9fe19e727cdf3f3768a42d4d3513
smssyquia <54983967+Deadify@users.noreply.github.com>
2020-05-01 17:29:10 +0800

    Fixed stylings that conflicted with the home page

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,51 +1,19 @@
 import React from "react";
-import db from '../backend/firebase.js'
 import "./Form.css";
 
-const uuidv4 = require("uuid/v4")
-
-
-
 function Home() {
-
-  const regionRef = React.createRef()
-  const countryRef = React.createRef()
-  const organizationRef = React.createRef()
-  const linkRef = React.createRef()
-
-  const onSubmitHandler = (event) => {
-    event.preventDefault()
-    // access input fields: ...ref.current.value
-    // post data to firebase firestore
-
-    const uniqueId = countryRef.current.value + "_" + uuidv4()
-    db.collection("donation-links").doc(uniqueId).set({
-      country: countryRef.current.value,
-      url: linkRef.current.value,
-      organization: organizationRef.current.value,
-      region: regionRef.current.value
-    }).then(() => {
-      console.log("success")
-      alert("SUCCESS! Your suggestion is noted!")
-      window.location.reload()
-    }).catch((error) => {
-      alert(error)
-      window.location.reload()
-    })
-  }
-
   return (
     <div class="donation_form">
       <title>Donate to Fight COVID-19</title>
-      <header>
+      <header class="form">
         <h1>Share how we can help your country fight COVID-19!</h1>
       </header>
-      <form onSubmit={onSubmitHandler}>
+      <form>
         <div class="group">
           <label id="region" for="region">
             Region
           </label>
-          <select name="region" class="field" required ref = {regionRef}>
+          <select name="region" class="field" required>
             <option disabled selected value>
               Select an option
             </option>
@@ -71,7 +39,6 @@ function Home() {
             name="country"
             class="field"
             placeholder="Enter a country"
-            ref = {countryRef}
           />
         </div>
 
@@ -85,7 +52,6 @@ function Home() {
             name="org"
             class="field"
             placeholder="Enter an organization name"
-            ref = {organizationRef}
           />
         </div>
 
@@ -102,7 +68,6 @@ function Home() {
             class="field"
             placeholder="Enter a link"
             required
-            ref = {linkRef}
           />
         </div>
         <br />
