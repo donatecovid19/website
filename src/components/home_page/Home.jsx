b61ec5c9b65e695190e81a5e297f103d4aafa899
smssyquia <54983967+Deadify@users.noreply.github.com>
2020-05-15 22:21:27 +0800

    Update Home.jsx

--- src/components/home_page/Home.jsx
+++ src/components/home_page/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Home.css";
 import { Event } from "../tracking";
-import db from "../../backend/firebase.js";
-
+import db from '../../backend/firebase.js'
+import alphabetSort from '../AlphabetSort'
 // import { render } from "@testing-library/react"; // why is this imported?
 
 // TODO: update the database with the information in the dummy constants :::
@@ -77,83 +77,80 @@ const NorthAmerica = (props) => {
 };
 
 class Home extends React.Component {
+  
   state = {
-    global: [],
-    asia: [],
-    europe: [],
-    northAmerica: [],
-    centralAmerica: [],
-    southAmerica: [],
-    africa: [],
-    oceania: [],
-    middleEast: [],
-  };
-
-  componentDidMount() {
-    db.collection("all_continents")
-      .get()
-      .then((snapshot) => {
-        var GLOBAL2 = [];
-        var ASIA2 = [];
-        var EUROPE2 = [];
-        var NORTH_AMERICA2 = [];
-        var CENTRAL_AMERICA2 = [];
-        var SOUTH_AMERICA2 = [];
-        var AFRICA2 = [];
-        var OCEANIA2 = [];
-        var MIDDLE_EAST2 = [];
-
-        snapshot.forEach((doc) => {
-          const data = doc.data();
-          switch (data.group) {
-            case "Global":
-              GLOBAL2 = data.links;
-              break;
-            case "Asia":
-              ASIA2 = data.links;
-              break;
-            case "Europe":
-              EUROPE2 = data.links;
-              break;
-            case "North_America":
-              NORTH_AMERICA2 = data.links;
-              break;
-            case "South_America":
-              SOUTH_AMERICA2 = data.links;
-              break;
-            case "Central_America":
-              CENTRAL_AMERICA2 = data.links;
-              break;
-            case "Africa":
-              AFRICA2 = data.links;
-              break;
-            case "Oceania":
-              OCEANIA2 = data.links;
-              break;
-            default:
-              // Middle_East
-              MIDDLE_EAST2 = data.links;
-          }
-          console.log(data);
-        });
-
-        this.setState({
-          global: GLOBAL2,
-          asia: ASIA2,
-          europe: EUROPE2,
-          northAmerica: NORTH_AMERICA2,
-          centralAmerica: CENTRAL_AMERICA2,
-          southAmerica: SOUTH_AMERICA2,
-          africa: AFRICA2,
-          oceania: OCEANIA2,
-          middleEast: MIDDLE_EAST2,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+      global: [],
+      asia: [],
+      europe: [],
+      northAmerica: [],
+      centralAmerica: [],
+      southAmerica: [],
+      africa: [],
+      oceania: [],
+      middleEast: []
   }
 
+  componentDidMount() {
+    db.collection('all_continents').get().then( snapshot => {
+
+      var GLOBAL2 = []
+      var ASIA2 = []
+      var EUROPE2 = []
+      var NORTH_AMERICA2 = []
+      var CENTRAL_AMERICA2 = []
+      var SOUTH_AMERICA2 = []
+      var AFRICA2 = []
+      var OCEANIA2 = []
+      var MIDDLE_EAST2 = []
+
+      snapshot.forEach( doc => {
+        const data = doc.data()
+        switch (data.group) {
+          case "Global":
+            GLOBAL2 = alphabetSort(data.links)
+            break
+          case "Asia":
+            ASIA2 = alphabetSort(data.links)
+            break
+          case "Europe":
+            EUROPE2 = alphabetSort(data.links)
+            break
+          case "North_America":
+            NORTH_AMERICA2 = alphabetSort(data.links)
+            break
+          case "South_America":
+            SOUTH_AMERICA2 = alphabetSort(data.links)
+            break
+          case "Central_America":
+            CENTRAL_AMERICA2 = alphabetSort(data.links)
+            break
+          case "Africa":
+            AFRICA2 = alphabetSort(data.links)
+            break
+          case "Oceania":
+            OCEANIA2 = alphabetSort(data.links)
+            break
+          default:
+            // Middle_East
+            MIDDLE_EAST2 = alphabetSort(data.links)
+        }
+        console.log(data)
+      })
+
+      this.setState({
+        global: GLOBAL2,
+        asia: ASIA2,
+        europe: EUROPE2,
+        northAmerica: NORTH_AMERICA2,
+        centralAmerica: CENTRAL_AMERICA2,
+        southAmerica: SOUTH_AMERICA2,
+        africa: AFRICA2,
+        oceania: OCEANIA2,
+        middleEast: MIDDLE_EAST2
+      })
+
+  }).catch( error => {console.log(error)})}
+              
   render() {
     return (
       <div className="home">
@@ -274,49 +271,17 @@ class Home extends React.Component {
         </div>
 
         <div class="grid">
-          <Box
-            name="Global"
-            list_of_links={this.state.global}
-            className="global"
-          />
+          <Box name="Global" list_of_links={this.state.global} className="global" />
           <Box name="Asia" list_of_links={this.state.asia} className="asia" />
-          <Box
-            name="Europe"
-            list_of_links={this.state.europe}
-            className="europe"
-          />
-
-          <NorthAmerica
-            name="North America"
-            list_of_links={this.state.northAmerica}
-            className="northamerica"
-          />
-          <Box
-            name="Central America"
-            list_of_links={this.state.centralAmerica}
-            className="centralamerica"
-          />
-          <Box
-            name="South America"
-            list_of_links={this.state.southAmerica}
-            className="southamerica"
-          />
-
-          <Box
-            name="Africa"
-            list_of_links={this.state.africa}
-            className="africa"
-          />
-          <Box
-            name="Oceania"
-            list_of_links={this.state.oceania}
-            className="oceania"
-          />
-          <Box
-            name="Middle East"
-            list_of_links={this.state.middleEast}
-            className="middleeast"
-          />
+          <Box name="Europe" list_of_links={this.state.europe} className="europe" />
+  
+          <NorthAmerica name="North America" list_of_links={this.state.northAmerica} className="northamerica" />
+          <Box name="Central America" list_of_links={this.state.centralAmerica} className="centralamerica" />
+          <Box name="South America" list_of_links={this.state.southAmerica} className="southamerica" />
+  
+          <Box name="Africa" list_of_links={this.state.africa} className="africa" />
+          <Box name="Oceania" list_of_links={this.state.oceania} className="oceania" />
+          <Box name="Middle East" list_of_links={this.state.middleEast} className="middleeast" />
         </div>
       </div>
     );
